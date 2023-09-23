b6adee94d668a46f7d382fd9331ccbb6ff80f60e
ddanov89 <138501928+ddanov89@users.noreply.github.com>
2023-09-23 18:36:12 +0300

    Fourth batch addedd

--- Exercises/sumDigits.js
+++ Exercises/sumDigits.js
@@ -1,9 +1,21 @@
 function sumDigits(num) {
+    let sum = 0;
+    while (num != 0) {
+        let units = num % 10;
+        num -= units;
+        num /= 10;
+        sum += units;
+    }
+    console.log(sum);
+}
+sumDigits(543);
+/*
+function sumDigits(num) {
     let numAsString = String(num); // num.String()
     let sum = 0;
-    for (let i = 0; i < numAsString.length; i++){
+    for (let i = 0; i < numAsString.length; i++) {
         sum += Number(numAsString[i]);
     }
     console.log(sum);
 }
-sumDigits(245678);
+sumDigits(245678);*/
