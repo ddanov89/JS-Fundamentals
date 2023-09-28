b176544b3ce82d7be0124608b8fed4b8262dee72
ddanov89 <138501928+ddanov89@users.noreply.github.com>
2023-09-28 08:06:32 +0300

    Daily contribution

---
+++ Exercises/townInfo.js
@@ -0,0 +1,8 @@
+(name, pop, area) => {`Town ${name} has population of ${pop} and area ${area} square km.`};
+('Sofia', 1286383, 492);
+/*
+function townInfo(name , pop, area) {
+    console.log(`Town ${name} has population of ${pop} and area ${area} square km.`);
+    
+}
+townInfo('Sofia', 1286383, 492);*/
