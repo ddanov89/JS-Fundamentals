11719ee0aaf02a3ecc3466f8879bf445cfe5288a
ddanov89 <138501928+ddanov89@users.noreply.github.com>
2024-01-12 22:42:40 +0200

    File added

---
+++ Exercises/monthlyReport.js
@@ -0,0 +1,7 @@
+function monthlyReport(arr) {
+    let index = 0;
+    
+}
+monthlyReport(["Deliver Micro 10000.00", "Sell Nick 500.00", "Sell Antony 260.50", "Deliver Micro 2000.50", "End"]);
+// monthlyReport(["Deliver North 200.30", "Sell Peter 30.20", "Return Macro 5000.00", "Return North 100.30", "Sell Peter 50.50", "End "]);
+// monthlyReport(["Deliver North 200.30","Deliver Micro 10000.00", "Deliver North 150.50", "End"]);
