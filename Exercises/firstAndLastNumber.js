98ac5207c52380f8f49e82b50869402881e1378e
ddanov89 <138501928+ddanov89@users.noreply.github.com>
2024-02-18 07:09:38 +0200

    File firstAndLastNumber.js added

---
+++ Exercises/firstAndLastNumber.js
@@ -0,0 +1,6 @@
+function firstAndLastKNumbers(input) {
+    let k = input.shift();
+    console.log(input.slice(0, k).join(" "));
+    console.log(input.slice(-k).join(" "));
+}
+firstAndLastKNumbers([3, 6, 7, 8, 9]);
